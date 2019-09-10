66b6ce6a65b60c9ea2a14ab8f631ff2072d7ec6e
XZZMemory <1933850821@qq.com>
2019-09-10 21:31:53 +0800

    l

--- src/other/对象创建的方法/clone/CloneDemo.java
+++ src/other/对象创建的方法/clone/CloneDemo.java
@@ -1,12 +1,13 @@
 package other.对象创建的方法.clone;
 
 public class CloneDemo {
-    public static void main(String[] args)
-    {
-        Obj a=new Obj();
-        Obj b= (Obj) a.clone();
+    public static void main(String[] args) {
+        Obj a = new Obj();
+        Obj b = (Obj) a.clone();//浅复制
         b.chanegBirth();
-        System.out.println(a.getBirth());
-        System.out.println(b.getBirth());
+        Obj c = (Obj) a.getObject().clone();//深复制
+        System.out.println(a.toString() + " " + a.getBirth() + " " + a.getList().toString());
+        System.out.println(a.toString() + " " + b.getBirth() + " " + b.getList().hashCode());
+        System.out.println(c.toString() + " " + c.getBirth() + " " + c.getList().toString());
     }
 }
--- src/other/对象创建的方法/clone/Obj.java
+++ src/other/对象创建的方法/clone/Obj.java
@@ -1,14 +1,26 @@
 package other.对象创建的方法.clone;
 
-import java.util.Date;
+import java.util.*;
 
 /**
  * 1 继承Cloneable方法
  * 2 实现clone接口
  */
-public class Obj implements Cloneable
-{
-    private Date birth=new Date();
+public class Obj implements Cloneable {
+    private Date birth = new Date();
+    Vector<Integer> list = new Vector<>();
+
+    public Vector<Integer> getList() {
+        return list;
+    }
+
+    public Obj getObject() {
+        return this;
+    }
+
+    public void setList(Vector<Integer> list) {
+        this.list = list;
+    }
 
     public Date getBirth() {
         return birth;
@@ -17,22 +29,21 @@ public class Obj implements Cloneable
     public void setBirth(Date birth) {
         this.birth = birth;
     }
-    public void chanegBirth()
-    {
+
+    public void chanegBirth() {
         this.birth.setTime(89l);
     }
 
     @Override
-    public Object clone()
-    {
-        Obj o=null;
+    public Object clone() {
+        Obj o = null;
         try {
-            o=(Obj)super.clone();
-        }catch (CloneNotSupportedException e)
-        {
+            o = (Obj) super.clone();
+        } catch (CloneNotSupportedException e) {
             e.printStackTrace();
         }
-        o.setBirth((Date)this.getBirth().clone());
+        o.setBirth((Date) this.getBirth().clone());
+        o.setList((Vector<Integer>) this.getList().clone());
         return o;
     }
 }
