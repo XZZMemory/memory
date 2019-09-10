cd1a3b692dea0f2cf611a81572f0bef0b7dbbc3e
XZZMemory <1933850821@qq.com>
2019-09-10 22:04:08 +0800

    'c'

--- src/other/exception/catchException.java
+++ src/other/exception/catchException.java
@@ -1,38 +1,47 @@
 package other.exception;
 
+import java.util.Vector;
+
 public class catchException {
-    public static void main(String[] args)
-    {
-        ExceptionTest2();
+    public static void main(String[] args) {
+        Vector<Integer> vector = ExceptionTest2();
+        System.out.println("data:" + vector.elementAt(0));
     }
+
     //try-catch捕获异常
-    public static void ExceptionTest1()
-    {
+    public static void ExceptionTest1() {
+        int[] c = {1, 2};
+        int bx = c[10];
+
         try {
             int[] a = {1, 2};
             int b = a[10];
-        }
-        catch (ArrayIndexOutOfBoundsException e)
-        {
-            System.out.println("错误类型是："+e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("错误类型是：" + e);
         }
     }
+
     //finally
-    public static void ExceptionTest2()
-    {
-        int[] data={1,2};
+    public static Vector<Integer> ExceptionTest2() {
+        Vector<Integer> vector;
+        int[] data = {1, 2};
+        //System.out.println(data[11]);
         try {
             System.out.println(data[10]);
-        }
-        catch (ArrayIndexOutOfBoundsException e)
-        {
-            System.out.println("错误信息是："+e);
-        }
-        finally {
+            vector = new Vector<>();
+            vector.add(1);
+            return vector;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("错误信息是：" + e);
+        } finally {
             System.out.print("执行finally语句部分：");
-            data[0]=11;
-            System.out.print("data数组0位置存储的数据是："+data[0]);
+            vector = new Vector<>();
+            vector.add(4);
+            data[0] = 11;
+            System.out.println("data数组0位置存储的数据是：" + data[0]);
+            //return vector;
         }
-
+        System.out.println("执行最后");
+        return vector;
     }
 }
