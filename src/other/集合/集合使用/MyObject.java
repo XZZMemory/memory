b49aa95c3e68281516514676e6e2d822fee6c55a
xiaozhenzhen001 <xiaozhenzhen001@ke.com>
2020-10-22 14:37:39 +0800

    set test

---
+++ src/other/集合/集合使用/MyObject.java
@@ -0,0 +1,9 @@
+package other.集合.集合使用;
+
+/**
+ * Description: memory
+ * Created by xiaozhenzhen001
+ * on 2020/10/22
+ */
+public class MyObject {
+}
---
+++ src/other/集合/集合使用/Usage.java
@@ -0,0 +1,16 @@
+package other.集合.集合使用;
+
+        import java.util.Arrays;
+        import java.util.List;
+
+/**
+ * Description: memory
+ * Created by xiaozhenzhen001
+ * on 2020/10/22
+ */
+public class Usage {
+    public static void main(String[] args) {
+        MyObject myObject = null;
+        List<MyObject> list = Arrays.asList(myObject);
+    }
+}
